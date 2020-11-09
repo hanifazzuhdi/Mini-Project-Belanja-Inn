85afa53dc07e1a70b2ad0a67cb48fe024b634080
hanifazzuhdi <hnfhanif52@gmail.com>
2020-11-09 07:22:49 +0700

    update readme.md

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Category;
-use App\User;
+use App\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,5 +13,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Category::create([
+            'category_name' => 'Elektronik'
+        ]);
+
+        Category::create([
+            'category_name' => 'Komputer dan Aksesoris'
+        ]);
+
+        Category::create([
+            'category_name' => 'Handphone dan Aksesoris'
+        ]);
+
+        Category::create([
+            'category_name' => 'Perlengkapan Rumah'
+        ]);
+
+        Role::create([
+            'role_name' => 'user'
+        ]);
+
+        Role::create([
+            'role_name' => 'admin'
+        ]);
     }
 }
