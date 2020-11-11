d9f3efb11496928d4a250c5b1d34644cf87d0a84
hanifazzuhdi <hnfhanif52@gmail.com>
2020-11-11 14:06:26 +0700

    update project

--- app/Http/Controllers/Api/PublicController.php
+++ app/Http/Controllers/Api/PublicController.php
@@ -39,9 +39,9 @@ class PublicController extends Controller
         }
     }
 
-    public function showCategory(Product $product, $category_id)
+    public function showCategory(Category $category, $category_id)
     {
-        $products = ProductResource::collection($product->where('category_id', $category_id)->get());
+        $products = ProductResource::collection($category->where('id', $category_id)->get());
 
         if (count($products) != 0) {
             return $this->SendResponse('succes', 'Data success to loaded', $products, 200);
