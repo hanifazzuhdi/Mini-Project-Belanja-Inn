c2da3db4011dd35f327392f7cb27f093b593a29d
hanifazzuhdi <hnfhanif52@gmail.com>
2020-11-11 21:17:29 +0700

    update project

--- app/Http/Controllers/Api/PublicController.php
+++ app/Http/Controllers/Api/PublicController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\User;
-use App\Category;
 use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -14,15 +12,9 @@ class PublicController extends Controller
     public function index()
     {
         $products = ProductResource::collection(Product::all());
-        // $categories = Category::all('id', 'category_name');
-
-        $data = [
-            'products' => $products,
-            // 'categories' => $categories
-        ];
 
         try {
-            return $this->SendResponse('succes', 'Data success to loaded', $data, 200);
+            return $this->SendResponse('succes', 'Data success to loaded', $products, 200);
         } catch (\Throwable $th) {
             return $this->SendResponse('failed', 'Data failed to loaded', null, 500);
         }
@@ -30,10 +22,10 @@ class PublicController extends Controller
 
     public function show(Product $products, $id)
     {
-        $products = new ProductResource($products->find(($id)));
+        $product = new ProductResource($products->find(($id)));
 
         try {
-            return $this->SendResponse('succes', 'Data success to loaded', $products, 200);
+            return $this->SendResponse('succes', 'Data success to loaded', $product, 200);
         } catch (\Throwable $th) {
             return $this->SendResponse('failed', 'Data failed to loaded', null, 500);
         }
