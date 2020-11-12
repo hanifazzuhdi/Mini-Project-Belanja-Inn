ef9d833b8854f9592ee5b2bfa4df0fe512506cae
hanifazzuhdi <hnfhanif52@gmail.com>
2020-11-12 09:04:01 +0700

    edit seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Database\Seeder;
 use App\Category;
+use App\Product;
 use App\Role;
-
+use App\Shop;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -40,6 +43,60 @@ class DatabaseSeeder extends Seeder
 
         Role::create([
             'role_name' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'mujahid',
+            'email' => 'mujahid@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        User::create([
+            'name' => 'fauzil',
+            'email' => 'fauzil@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        Shop::create([
+            'id' => 1,
+            'shop_id' => 1,
+            'shop_name' => 'mujahid shop',
+            'avatar' => 'sample.png',
+            'address' => 'jakarta indonesia',
+            'description' => 'ini adalah toko milik mujahid'
+        ]);
+
+        Shop::create([
+            'id' => 2,
+            'shop_id' => 2,
+            'shop_name' => 'fauzil shop',
+            'avatar' => 'sample.png',
+            'address' => 'palembang indonesia',
+            'description' => 'ini adalah toko milik fauzil'
+        ]);
+
+        Product::create([
+            'product_name' => 'energen rasa milo',
+            'price' => '15.000',
+            'quantity' => 10,
+            'description' => 'ini adalah energen rasa milo',
+            'image' => 'sample.png',
+            'weight' => '1kg',
+            'sold' => 10,
+            'shop_id' => 2,
+            'category_id' => 1
+        ]);
+
+        Product::create([
+            'product_name' => 'energen rasa Pisang',
+            'price' => '19.000',
+            'quantity' => 10,
+            'description' => 'ini adalah energen rasa pisang',
+            'image' => 'sample.png',
+            'weight' => '10kg',
+            'sold' => 100,
+            'shop_id' => 1,
+            'category_id' => 4
         ]);
     }
 }
