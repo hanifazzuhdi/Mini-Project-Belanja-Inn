6dea45e5687984440ec92673388cb34b900e08db
hanifazzuhdi <hnfhanif52@gmail.com>
2020-11-12 09:03:36 +0700

    hanif => add usercontroller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function getUserAuth(User $user)
+    {
+        $data = $user->find(Auth::id());
+
+        return response([
+            'status' => 'success',
+            'data' => $data
+        ], 200);
+    }
+
+    public function updateUser(Request $request)
+    {
+        $data = User::find(Auth::id());
+
+        dd($data);
+    }
 }
