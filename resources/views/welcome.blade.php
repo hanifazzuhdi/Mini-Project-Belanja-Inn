46f5048740185eff113eee6d3cde1dd584561066
hanifazzuhdi <hnfhanif52@gmail.com>
2020-11-14 14:27:03 +0700

    hanif => create auth web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,9 +69,8 @@
                 <div class="top-right links">
                     @auth
 
-                    @if (auth()->user()->role_id == 3){
+                    @if (auth()->user()->role_id == 3)
                         <a href="{{ url('/home') }}">Home</a>
-                    }
                     @endif
 
                     <a href="{{ route('logout') }}"
