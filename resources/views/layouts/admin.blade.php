c6e656f230c7407d57488f55877a93cdf1f80b62
hanifazzuhdi <hnfhanif52@gmail.com>
2020-11-15 18:49:22 +0700

    Revert "update"
    This reverts commit dd112c6599cf8af60d1fd363f9c9b94e717b0c14.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,12 +12,12 @@
     <title>Admin D-Shopee</title>
 
 
-    <link href=" {{url('Admin/vendor/fontawesome-free/css/all.min.css')}} " rel="stylesheet" type="text/css">
+    <link href=" {{url('Admin/components/fontawesome-free/css/all.min.css')}} " rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <link href=" {{url('Admin/css/sb-admin-2.min.css')}} " rel="stylesheet">
 
-    <link href=" {{url ('Admin/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+    <link href=" {{url ('Admin/components/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 
     <link rel="stylesheet" href="/css/mystyle.css">
 </head>
@@ -403,21 +403,21 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src=" {{url('Admin/vendor/jquery/jquery.min.js')}}"></script>
-    <script src=" {{url('Admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}} "></script>
+    <script src=" {{url('Admin/components/jquery/jquery.min.js')}}"></script>
+    <script src=" {{url('Admin/components/bootstrap/js/bootstrap.bundle.min.js')}} "></script>
 
     <!-- Core plugin JavaScript-->
-    <script src=" {{url('Admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <script src=" {{url('Admin/components/jquery-easing/jquery.easing.min.js')}}"></script>
 
     <!-- Custom scripts for all pages-->
     <script src=" {{url('Admin/js/sb-admin-2.min.js')}}"></script>
 
     <!-- Page level plugins -->
-    <script src=" {{url('Admin/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src=" {{url('Admin/components/chart.js/Chart.min.js')}}"></script>
 
     <!-- Page level plugins for table -->
-    <script src=" {{url('Admin/vendor/datatables/jquery.dataTables.min.js')}} "></script>
-    <script src=" {{url('Admin/vendor/datatables/dataTables.bootstrap4.min.js')}} "></script>
+    <script src=" {{url('Admin/components/datatables/jquery.dataTables.min.js')}} "></script>
+    <script src=" {{url('Admin/components/datatables/dataTables.bootstrap4.min.js')}} "></script>
     <script src=" {{url('Admin/js/demo/datatables-demo.js')}} "></script>
 
     <!-- Page level custom scripts -->
