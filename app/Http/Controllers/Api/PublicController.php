b84248bec2741efeab34579002896eb53bc25431
Fauzil14 <fauzilazim39@gmail.com>
2020-11-23 09:39:22 +0700

    fauzil => search with filter

--- app/Http/Controllers/Api/PublicController.php
+++ app/Http/Controllers/Api/PublicController.php
@@ -47,10 +47,11 @@ class PublicController extends Controller
 
     public function search(Request $request)
     {
-        // if($request->keyword == null ) {
+        // if(empty($request->keyword)) {
         //     return $this->SendResponse('failed', 'There is no input in search box', null, 400);
         // };
-
+        
+        /* filters = ['price', 'Ready Stock', 'sort_by'] */
         $filters = $request->filterBy;
 
         $query = Product::query();
@@ -61,7 +62,7 @@ class PublicController extends Controller
         $products = $query->when($request->keyword, function ($query) use ($request) {
             return $query->where('product_name', 'like', "%{$request->keyword}%")
                 ->orWhereHas('category', function ($query) use ($request) {
-                   return $query->where('category_name', 'like', "%{$request->keyword}%");
+                    return $query->where('category_name', 'like', "%{$request->keyword}%");
                 });
         })
             ->join('shops', 'products.shop_id', '=', 'shops.id')
@@ -70,53 +71,65 @@ class PublicController extends Controller
             ->get()
             ->toArray();
 
-
-        /* filters = ['price', 'Ready Stock', 'sort_by'] */
-
         $products = collect($products);
-        /* price range */
 
-        // costum price range
-        $pr4 = null;
-        if (!empty($filters['price']['range']) && is_array($filters['price']['range'])) {
-            $pr4 = [(int) $filters['price']['range']['top'], (int) $filters['price']['range']['bottom']];
-        }
-
-        $pr = [
-            'pr1' => [0, 75000],
-            'pr2' => [75000, 150000],
-            'pr3' => [150000, 200000],
-            'pr4' => $pr4
-        ];
-
-        $sort_by = [
-            'terlaris' => ['sold', true],
-            'termahal' => ['price', true],
-            'termurah' => ['price', false]
-        ];
-
-        if (!empty($filters['price']['range']) && is_string($filters['price']['range'])) {
-            $products = $products->whereBetween('price', $pr["{$filters['price']['range']}"]);
-        } elseif (!empty($filters['price']['range'])) {
-            $products = $products->whereBetween('price', $pr['pr4']);
-        }
-
-        if (!empty($filters['stock_status']) && $filters['stock_status'] == 'ready') {
-            $products = $products->where('quantity', '>', 0);
-        }
-
-        if (!empty($filters['sort_by']) && $filters['sort_by'] != 'terbaru') {
-            if (Arr::exists($sort_by, $filters['sort_by'])) {
-                $key = $sort_by["{$filters['sort_by']}"][0];
-                $sortmode = $sort_by["{$filters['sort_by']}"][1];
-                $products = $products->sortBy($key, SORT_REGULAR, $sortmode); // parameter ke 3 [true => descending, false => ascending]
+        if(!empty($filters)) {
+            /* price range */
+            // costum price range
+            $pr4 = null;
+            if (!empty($filters['price']['range']) && is_array($filters['price']['range'])) {
+                $pr4 = [(int) $filters['price']['range']['top'], (int) $filters['price']['range']['bottom']];
             }
+
+            $pr = [
+                'pr1' => [0, 75000],
+                'pr2' => [75000, 150000],
+                'pr3' => [150000, 200000],
+                'pr4' => $pr4
+            ];
+
+            $sort_by = [
+                'terlaris' => ['sold', true],
+                'termahal' => ['price', true],
+                'termurah' => ['price', false]
+            ];
+
+            if (!empty($filters['price']['range']) && is_string($filters['price']['range'])) {
+                $products = $products->whereBetween('price', $pr["{$filters['price']['range']}"]);
+            } elseif (!empty($filters['price']['range'])) {
+                $products = $products->whereBetween('price', $pr['pr4']);
+            }
+
+            if (!empty($filters['stock_status']) && $filters['stock_status'] == 'ready') {
+                $products = $products->where('quantity', '>', 0);
+            }
+
+            if (!empty($filters['sort_by']) && $filters['sort_by'] != 'terbaru') {
+                if (Arr::exists($sort_by, $filters['sort_by'])) {
+                    $key = $sort_by["{$filters['sort_by']}"][0];
+                    $sortmode = $sort_by["{$filters['sort_by']}"][1];
+                    $products = $products->sortBy($key, SORT_REGULAR, $sortmode); // parameter ke 3 [true => descending, false => ascending]
+                }
+            }
+            
+            /* filters end */
         }
+        $products = $products->map(function ($value, $key) {
+            $value['price'] = number_format($value['price'], 0, ',', '.');
+            return $value;
+        });
 
-        $filtered = $products->values()->all();
+        $data = $products->values()->all();
 
-        if (count($filtered) != 0) {
-            return $this->SendResponse('succes', 'Data loaded successfully', $filtered, 200);
+        if (count($data) != 0) {
+            return $this->SendResponse('succes', 'data loaded successfully', $data, 200);
         } else return $this->SendResponse('failed', 'Data failed to load', null, 500);
     }
 }
+
+
+/* Note: map()
+    map method is used to iterate through the full collection. It accepts a callback as an argument. 
+    value and the key is passed to the callback. Callback can modify the values and return them. 
+    Finally, a new collection instance of modified items is returned.
+*/
