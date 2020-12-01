da91954d12bf3fdda668e9bf61855a2edd852efb
hanifazzuhdi <hnfhanif52@gmail.com>
2020-12-01 07:07:42 +0700

    update

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,12 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Cart;
 use App\Order;
-use Exception;
 use App\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\CartResource;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\OrderResource;
 
