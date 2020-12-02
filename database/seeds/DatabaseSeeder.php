0cf508b67c8b918915595b699250d42d67d262eb
hanifazzuhdi <hnfhanif52@gmail.com>
2020-12-02 21:46:10 +0700

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -63,6 +63,14 @@ class DatabaseSeeder extends Seeder
         ]);
 
         User::create([
+            'name' => 'faiz',
+            'username' => 'faiz01',
+            'email' => 'faiz@gmail.com',
+            'password' => Hash::make('password'),
+            'role_id' => 2
+        ]);
+
+        User::create([
             'name' => 'Hanif',
             'username' => 'hanif',
             'email' => 'hanif@gmail.com',
@@ -92,62 +100,54 @@ class DatabaseSeeder extends Seeder
             'user_id' => 2,
             'shop_name' => 'fauzil shop',
             'avatar' => 'https://via.placeholder.com/150',
-            'address' => 'palembang indonesia',
+            'address' => 'padang indonesia',
             'description' => 'ini adalah toko milik fauzil'
         ]);
 
         Shop::create([
             'id' => 3,
             'user_id' => 3,
-            'shop_name' => 'hanif shop',
+            'shop_name' => 'faiz shop',
             'avatar' => 'https://via.placeholder.com/150',
-            'address' => 'DIY indonesia',
-            'description' => 'ini adalah toko milik hanif'
+            'address' => 'papua indonesia',
+            'description' => 'ini adalah toko milik faiz'
         ]);
 
-        Shop::create([
-            'id' => 4,
-            'user_id' => 4,
-            'shop_name' => 'usman shop',
-            'avatar' => 'https://via.placeholder.com/150',
-            'address' => 'DIY indonesia',
-            'description' => 'ini adalah toko milik usman'
-        ]);
 
-        Product::create([
-            'product_name' => 'Energen rasa milo',
-            'price' => 150000,
-            'quantity' => 10,
-            'description' => 'ini adalah energen rasa milo',
-            'image' => 'https://via.placeholder.com/150',
-            'weight' => '1kg',
-            'sold' => 0,
-            'shop_id' => 2,
-            'category_id' => 1
-        ]);
+        // Product::create([
+        //     'product_name' => 'Energen rasa milo',
+        //     'price' => 150000,
+        //     'quantity' => 10,
+        //     'description' => 'ini adalah energen rasa milo',
+        //     'image' => 'https://via.placeholder.com/150',
+        //     'weight' => '1kg',
+        //     'sold' => 0,
+        //     'shop_id' => 2,
+        //     'category_id' => 1
+        // ]);
 
-        Product::create([
-            'product_name' => 'Energen rasa Pisang',
-            'price' => 19000,
-            'quantity' => 10,
-            'description' => 'ini adalah energen rasa pisang',
-            'image' => 'https://via.placeholder.com/150',
-            'weight' => '10kg',
-            'sold' => 0,
-            'shop_id' => 1,
-            'category_id' => 4
-        ]);
+        // Product::create([
+        //     'product_name' => 'Energen rasa Pisang',
+        //     'price' => 19000,
+        //     'quantity' => 10,
+        //     'description' => 'ini adalah energen rasa pisang',
+        //     'image' => 'https://via.placeholder.com/150',
+        //     'weight' => '10kg',
+        //     'sold' => 0,
+        //     'shop_id' => 1,
+        //     'category_id' => 4
+        // ]);
 
-        Product::create([
-            'product_name' => 'Energen rasa Kopi Hitam',
-            'price' => 20000,
-            'quantity' => 10,
-            'description' => 'ini adalah energen rasa Kopi',
-            'image' => 'https://via.placeholder.com/150',
-            'weight' => '10kg',
-            'sold' => 0,
-            'shop_id' => 2,
-            'category_id' => 4
-        ]);
+        // Product::create([
+        //     'product_name' => 'Energen rasa Kopi Hitam',
+        //     'price' => 20000,
+        //     'quantity' => 10,
+        //     'description' => 'ini adalah energen rasa Kopi',
+        //     'image' => 'https://via.placeholder.com/150',
+        //     'weight' => '10kg',
+        //     'sold' => 0,
+        //     'shop_id' => 2,
+        //     'category_id' => 4
+        // ]);
     }
 }
