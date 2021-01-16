de9810da73fc8c60363cfbcc902318867ff000d6
hanifazzuhdi <hnfhanif52@gmail.com>
2021-01-16 11:03:14 +0700

    update config mail

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME', 'biruwarna383@gmail.com'),
-            'password' => env('MAIL_PASSWORD', 'fzpigbjuverrxjsm'),
+            'host' => env('MAIL_HOST'),
+            'port' => env('MAIL_PORT'),
+            'encryption' => env('MAIL_ENCRYPTION'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
         ],
